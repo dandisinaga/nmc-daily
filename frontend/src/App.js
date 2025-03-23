c7311ff3e5f4679324b1d0bde37a9cc957c5196f
Mochamad Dandi <dandisinaga@icloud.com>
2025-03-24 02:24:11 +0700

    err

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     
     <Routes>
-      <Route path="/daily" element={<Login />} />
+      <Route path="/" element={<Login />} />
       <Route path="/daily-activity" element={<SendEmail />} />
       <Route path="/logout" element={<Logout />} />
     </Routes>
